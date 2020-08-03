0a42d560d6cfb2b3a450b61148d70c0fb3a16114
Sato1998 <69139466+Sato1998@users.noreply.github.com>
2020-08-04 00:08:48 +0900

    Create kadai.js

---
+++ kadai.js
@@ -0,0 +1,25 @@
+/* ・FizzBuzz問題
+1から20までの数を表示するスクリプトを書いてください。
+ただし3の倍数のときは数の代わりに Fizz と、5の倍数のときは Buzz と表示し、
+3と5両方の倍数の場合には FizzBuzz と表示してください。*/
+
+
+for(var i=1;i<=20;i++){
+
+  if(i%3==0 && i%5==0){
+    // 3と5の倍数の時の処理
+    console.log("FizzBuzz");
+  }
+  else if(i%3==0){
+    // 3の倍数の時の処理
+    console.log("Fizz");
+  }
+  else if(i%5==0){
+    // 5の倍数の時の処理
+    console.log("Buzz");
+    }
+  else {
+    // どれにも当てはまらないとき
+    console.log(i);
+  }
+}
